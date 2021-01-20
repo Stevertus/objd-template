796ab425405dac661d0884794ffb09bb8f4a8a4d
focccus <marten.winkler@wtnet.de>
2021-01-20 21:32:11 +0100

    updated to use annotations

---
+++ lib/pack/starter_pack.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'starter_pack.dart';
+
+// **************************************************************************
+// FileGenerator
+// **************************************************************************
+
+final File MainFile = File(
+  'starter/pack/main',
+  child: main,
+);
+
+final File LoadFile = File(
+  'starter/pack/load',
+  child: load,
+);
+
+// **************************************************************************
+// PackGenerator
+// **************************************************************************
+
+class StarterPack extends Widget {
+  @override
+  Widget generate(Context context) => Pack(
+        name: 'objdstart',
+        files: starter,
+      );
+}
